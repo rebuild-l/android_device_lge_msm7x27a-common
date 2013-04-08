8afc469214eeb7295b9ac3154ab5e8ab3dd008cc
Matthew Xie <mattx@google.com>
2013-04-08 10:43:35 -0700

    New call-in/-back functions for Controller to do  vendor-specific shutdown
    A pair of asynchronous call-in and call-back API are added into the Host
    Controller Vendor Lib interface (bt_vendor_lib.h). The caller calls this new
    call-in function to inform the vendor module to perform vendor-specific
    shutdown process (e.g. send HCI_RESET to BT Controller) before the caller calls
    for interface cleanup() function. vendor module is responsible for calling
    call-back function to notify the caller completion of vendor-specific shutdown
    process.
    bug 7390787
    
    Change-Id: I6db7bd6252635805b057df172d99ec5438945b9b

--- libbt-vendor/src/bt_vendor_qcom.c
+++ libbt-vendor/src/bt_vendor_qcom.c
@@ -46,6 +46,11 @@ int pFd = 0;
 bt_vendor_callbacks_t *bt_vendor_cbacks = NULL;
 uint8_t vnd_local_bd_addr[6]={0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
+#if (HW_NEED_END_WITH_HCI_RESET == TRUE)
+void hw_epilog_process(void);
+#endif
+
+
 /******************************************************************************
 **  Local type definitions
 ******************************************************************************/
@@ -169,6 +174,18 @@ static int op(bt_vendor_opcode_t opcode, void *param)
             break;
 
         case BT_VND_OP_LPM_WAKE_SET_STATE:
+            break;
+        case BT_VND_OP_EPILOG:
+            {
+#if (HW_NEED_END_WITH_HCI_RESET == FALSE)
+                if (bt_vendor_cbacks)
+                {
+                    bt_vendor_cbacks->epilog_cb(BT_VND_OP_RESULT_SUCCESS);
+                }
+#else
+                hw_epilog_process();
+#endif
+            }
             break;
     }
 
