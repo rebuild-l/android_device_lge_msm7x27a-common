e5a256a0dc4a3fd2a8d22a607e962b02e8c7cd81
RonGokhale <cips173@gmail.com>
2013-09-12 16:36:54 +0530

    msm7x27a-common: emulate internal sd
    Change-Id: I798dd33002e51aee9f12ca0d6fb4a393fd1d59ce

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -170,7 +170,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.mem.max_hidden_apps=10 \
     debug.gr.numframebuffers=3 \
     ro.bq.gpu_to_cpu_unsupported=1 \
-    dalvik.vm.dexopt-data-only=1
+    dalvik.vm.dexopt-data-only=1 \
+    ro.fuse_sdcard=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
    media.stagefright.enable-player=true \
