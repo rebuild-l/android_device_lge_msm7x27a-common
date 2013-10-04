redmi2/android_hardware_qcom_audio

audio/msm7627a: Move LPA memory to bootloader heap
- Reduce the size of LPA buffer size to 496KB to accomodate
  it in bootloader heap and accordingly update the heap id

Change-Id: Idccc3542ca385e846925d2a6e006b298a2bc27f7

msm7627a/AudioHardware_cad.cpp
@@ -2948,7 +2948,7 @@ void* AudioHardware::AudioSessionOutLPA::memBufferAlloc(int nSize, int32_t *ion_
 
     alloc_data.len =   nSize;
     alloc_data.align = 0x1000;
-    alloc_data.flags = ION_HEAP(ION_AUDIO_HEAP_ID);
+    alloc_data.flags = ION_HEAP(ION_AUDIO_HEAP_BL_ID);
     int rc = ioctl(ionfd, ION_IOC_ALLOC, &alloc_data);
     if (rc) {
         ALOGE("ION_IOC_ALLOC ioctl failed\n");

rebuild-l/android_device_lge_msm7x27a-common

Change-Id: I06ceec06beb8b68e47a2041125019f03ff7484e6

libaudio/AudioHardware_cad.cpp
@@ -3007,7 +3007,7 @@ void* AudioHardware::AudioSessionOutLPA::memBufferAlloc(int nSize, int32_t *ion_
 
     alloc_data.len =   nSize;
     alloc_data.align = 0x1000;
-    alloc_data.heap_mask = ION_HEAP(ION_AUDIO_HEAP_ID);
+    alloc_data.heap_mask = ION_HEAP(ION_AUDIO_HEAP_BL_ID);
     alloc_data.flags = 0;
     int rc = ioctl(ionfd, ION_IOC_ALLOC, &alloc_data);
     if (rc) {