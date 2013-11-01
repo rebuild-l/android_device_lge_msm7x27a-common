3b907775adc7d675d8f152022832a6dcd3595479
RonGokhale <cips173@gmail.com>
2013-11-01 14:56:48 +0530

    msm7x27a-common: remove torch config
    Change-Id: I6092611a23a4f7ff70327a2cfde0aab3627b654e

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -110,9 +110,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libaudioutils \
     audio.a2dp.default \
-    audio.usb.default \
-    audio.primary.msm7x27a \
-    audio_policy.msm7x27a
+    audio.usb.default
 
 # SoftAP files 
 PRODUCT_PACKAGES += \
