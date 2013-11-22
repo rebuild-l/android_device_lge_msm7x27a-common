3f3da9ea1668c394c4d71722485745dab58bdf81
RonGokhale <cips173@gmail.com>
2013-11-22 18:47:55 +0530

    msm7x27a-common: enable lpa for l5
    Change-Id: If717f25803294a6516d555afe6a8db78eae262e3

--- rootdir/etc/init.lge.target.sh
+++ rootdir/etc/init.lge.target.sh
@@ -31,7 +31,7 @@ device=`getprop ro.product.device`
 case $device in
      e610 | e612 | e615 | m4)
         setprop ro.sf.lcd_density 160
-        setprop lpa.decode false
+        setprop lpa.decode true
     ;;
 esac
 
