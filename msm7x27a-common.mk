513aac4d3b3d4ebd2a472e156837e4ac40255bd2
RonGokhale <cips173@gmail.com>
2014-02-28 23:57:52 +0530

    msm7x27a-common: move nfc stuff to their own makefile
    Change-Id: I60d5c71ef2733f0219a069dce477dc7e6b6f99d5

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -69,27 +69,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw \
     libdashplayer
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
-
-# NFC packages
-PRODUCT_PACKAGES += \
-    libnfc \
-    libnfc_jni \
-    Nfc \
-    Tag \
-    com.android.nfc_extras
-
-# NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := device/lge/msm7x27a-common/nfc/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := device/lge/msm7x27a-common/nfc/nfcee_access_debug.xml
-endif
-PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
-
 # Gps hal
 PRODUCT_PACKAGES += \
     gps.msm7x27a \
