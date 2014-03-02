84ca6078b7b0c7eba17cbc3bdf5711e00c854053
Cybo Jenix <anthonydking@gmail.com>
2014-03-02 16:17:38 +0000

    msm7x27a-common: optimise baseband detect script

--- rootdir/etc/init.qcom.baseband.sh
+++ rootdir/etc/init.qcom.baseband.sh
@@ -4,6 +4,9 @@
 PATH=/sbin:/system/sbin:/system/bin:/system/xbin
 export PATH
 
-v10=`strings /dev/block/mmcblk0p12 | grep -e "-V10.-"| head -1`
-v20=`strings /dev/block/mmcblk0p12 | grep -e "-V20.-"| head -1`
-setprop gsm.version.baseband ${v10}${v20}
+bbfile="/data/.baseband"
+if [ ! -f ${bbfile} ]
+then
+  echo `strings /dev/block/mmcblk0p12 | grep -e "-V10.-" -e "-V20.-" | head -1` > ${bbfile}
+fi
+setprop gsm.version.baseband `cat ${bbfile}`
