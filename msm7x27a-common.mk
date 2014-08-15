ad45becb05768728b5125f831696e6d551e5d840
RonGokhale <cips173@gmail.com>
2014-08-16 00:17:16 +0530

    msm7x27a-common: skip copying prebuilt wlan module and fw if QCPATH is set
     - we don't want prebuilt module in internal builds.
    
    Change-Id: Ibd87efd707f49967dfe7df909a6b30304d1aa70d

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -46,12 +46,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hostapd/hostapd_default.conf::system/etc/hostapd/hostapd_default.conf
 
 # Wlan
+ifeq ($(QCPATH),)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wlan/wlan.ko:system/lib/modules/wlan.ko \
     $(LOCAL_PATH)/wlan/firmware/WCN1314_qcom_wlan_nv.bin:system/etc/firmware/wlan/volans/WCN1314_qcom_wlan_nv.bin \
     $(LOCAL_PATH)/wlan/firmware/WCN1314_qcom_fw.bin:system/etc/firmware/wlan/volans/WCN1314_qcom_fw.bin \
     $(LOCAL_PATH)/wlan/firmware/WCN1314_cfg.dat:system/etc/firmware/wlan/volans/WCN1314_cfg.dat \
     $(LOCAL_PATH)/wlan/firmware/WCN1314_qcom_cfg.ini:system/etc/firmware/wlan/volans/WCN1314_qcom_cfg.ini
+endif
 
 # Permission files
 PRODUCT_COPY_FILES += \
